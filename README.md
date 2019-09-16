2b09e385e2123a4e629c05427a1fbdfc06b2b3ed
jay <jaynguyen2@outlook.com>
2019-09-16 14:16:47 +0700

    Rewrite App.js in React Hooks

--- README.md
+++ README.md
@@ -39,18 +39,3 @@ $ sudo npm install --unsafe-perm node-sass
 ## Version 1.0.0
 Rewrite to React Hook from <a href="https://reactjsexample.com/world-clock-to-include-the-day-of-the-week-and-the-weather/">this Reactjs example</a>
 
-## License
-
-    Copyright 2019 Anh Nguyen
-
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
-
-        http://www.apache.org/licenses/LICENSE-2.0
-
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
--- src/App.js
+++ src/App.js
@@ -1,40 +1,37 @@
 import React,
-        { Component, useState } from "react";
+        { Component, 
+          useState,
+          useEffect } from "react";
 import * as moment from 'moment';
 import City from "./components/city";
 
 
-class WeatherApp extends Component {
-  constructor() {
-    super();
-    this.state = {
-      currentTime: moment(),
-      cities: {
-        'San Mateo': {
-          weatherId: 5391959,
-          timeZone: 'America/Los_Angeles'
-        },
-        'Toronto' : {
-          weatherId: 6167865,
-          timeZone: 'America/Toronto'
-        },
-        'Paris': {
-          weatherId: 2988507,
-          timeZone: 'Europe/Paris'
-        },
-        'Sydney': {
-          weatherId: 2147714,
-          timeZone: 'Australia/Sydney'
-        }
-      }
-    }
-  }
-  componentDidMount() {
-    window.setInterval(() => this.setState({ currentTime: moment() }), 5000)
-  }
-  render() {
-    const { cities, currentTime } = this.state;
-    return (
+function WeatherApp() {
+  const [currentTime] = useState(moment());
+  const [cities] = useState({'Toronto' : {
+                                          weatherId: 6167865,
+                                          timeZone: 'America/Toronto'
+                                        }
+                                        ,
+                                          'Paris': {
+                                          weatherId: 2988507,
+                                          timeZone: 'Europe/Paris'
+                                        },
+                                        'Sydney': {
+                                          weatherId: 2147714,
+                                          timeZone: 'Australia/Sydney'
+                                        },
+                                        'San Mateo': {
+                                        weatherId: 5391959,
+                                        timeZone: 'America/Los_Angeles'
+                                        },
+                                      })                                   
+   // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    window.setInterval = currentTime/5000;
+  });
+
+  return (
       <div className="panels">
         {
           Object
@@ -50,7 +47,6 @@ class WeatherApp extends Component {
         }
       </div>
     )
-  }
 }
 
 export default WeatherApp;
